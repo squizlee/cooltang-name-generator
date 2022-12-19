99c9cad3f9ae6b5a9c2eb00dfbf3ad0c4370e85a
f1redr4g0n <>
2022-12-19 15:12:07 -0300

    added option for a custom separator between words

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 import adjectives from './data/adjectives.js'
 import nouns from './data/nouns.js'
 
-export function generate_name(maxNum = 0) {
+export function generate_name(maxNum = 0, separator = '') {
     const adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
     const noun = nouns[Math.floor(Math.random() * nouns.length)]
+    const randomNumber = Math.floor(Math.random() * maxNum + 1)
+
+    if (separator) {
+        return maxNum 
+        ? `${adjective}${separator}${noun}${separator}${randomNumber}`
+        : `${adjective}${separator}${noun}`
+    }
 
     return maxNum 
-    ? `${adjective} ${noun}${Math.floor(Math.random() * maxNum + 1)}`
+    ? `${adjective} ${noun}${randomNumber}`
     : `${adjective} ${noun}`
 }
