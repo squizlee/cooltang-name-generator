d5a3c9dc2db2f40630631c2497e6df1f63efc672
f1redr4g0n <>
2022-12-20 08:24:40 -0300

    removed redundancy by operating an array

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 import adjectives from './data/adjectives.js'
 import nouns from './data/nouns.js'
 
-export function generate_name(maxNum = 0, separator = '') {
+export function generate_name(maxNum = 0, separator = ' ') {
     const adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
     const noun = nouns[Math.floor(Math.random() * nouns.length)]
     const randomNumber = Math.floor(Math.random() * maxNum + 1)
 
-    if (separator) {
-        return maxNum 
-        ? `${adjective}${separator}${noun}${separator}${randomNumber}`
-        : `${adjective}${separator}${noun}`
-    }
-
-    return maxNum 
-    ? `${adjective} ${noun}${randomNumber}`
-    : `${adjective} ${noun}`
+    return maxNum
+    ? [adjective, noun, randomNumber].join(separator)
+    : [adjective, noun].join(separator)
 }
