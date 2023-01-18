2422f838acb7570eb758bddfc1fd063b215b12be
squizlee <ethanscavia777@gmail.com>
2023-01-18 19:04:02 +1100

    ✅ fix test
    - camelCase broke it woops

--- main.test.js
+++ main.test.js
@@ -1,9 +1,9 @@
-import { generate_name } from './main.js'
+import { generateName } from './main.js'
 import adjectives from './data/adjectives.js'
 import nouns from './data/nouns.js'
 
 it('should use an adjective and a noun from the selected data.', () => {
-  const name = generate_name()
+  const name = generateName()
   const adjective = name.split(' ')[0]
   const noun = name.split(' ')[1]
 
@@ -13,7 +13,7 @@ it('should use an adjective and a noun from the selected data.', () => {
 
 it('should create a random number from 1 to the maxNum argument provided.', () => {
   const maxNum = 5
-  const name = generate_name(maxNum)
+  const name = generateName(maxNum)
   const randomNumber = +name.split(' ')[2]
 
   expect(randomNumber).toBeGreaterThanOrEqual(1)
@@ -22,7 +22,7 @@ it('should create a random number from 1 to the maxNum argument provided.', () =
 
 it('should create three names separated by the same separator.', () => {
   const separator = '-'
-  const name = generate_name(5, separator)
+  const name = generateName(5, separator)
 
   expect(name.split(separator).length).toBe(3)
 })
