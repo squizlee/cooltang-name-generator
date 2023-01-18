155d73f45dd0b72b03d69204bc68b722b9188c5d
squizlee <ethanscavia777@gmail.com>
2023-01-18 18:55:49 +1100

    🎨 camel case

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import adjectives from './data/adjectives.js'
 import nouns from './data/nouns.js'
 
-export function generate_name(maxNum = 0, separator = ' ') {
+export function generateName(maxNum = 0, separator = ' ') {
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
   const noun = nouns[Math.floor(Math.random() * nouns.length)]
   const randomNumber = Math.floor(Math.random() * maxNum + 1)
