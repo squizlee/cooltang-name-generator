ecafe779ec55395db1c05ad4fb3077f2dee68e74
squizlee <ethanscavia777@gmail.com>
2023-01-18 18:54:16 +1100

    🎨 use formatter for consistency

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import adjectives from './data/adjectives.js'
 import nouns from './data/nouns.js'
 
 export function generate_name(maxNum = 0, separator = ' ') {
-    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
-    const noun = nouns[Math.floor(Math.random() * nouns.length)]
-    const randomNumber = Math.floor(Math.random() * maxNum + 1)
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
+  const noun = nouns[Math.floor(Math.random() * nouns.length)]
+  const randomNumber = Math.floor(Math.random() * maxNum + 1)
 
-    return maxNum
+  return maxNum
     ? [adjective, noun, randomNumber].join(separator)
     : [adjective, noun].join(separator)
 }
